af4232b85e3aadd26e8d80e8e9a29c8db2d28fbf
Robby Pelssers <robby.pelssers@engie.com>
2019-10-26 12:04:24 +0200

    add clarity design system

--- apps/starwars/src/app/app.module.ts
+++ apps/starwars/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ClarityModule } from "@clr/angular";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
